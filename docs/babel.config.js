776ae5f659f58e47d6eaa539f21c265a9b388bc9
Marcos <marcosvmmoura@gmail.com>
2018-07-01 19:46:32 -0300

    chore: update vue-cli

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   presets: [
-    '@vue/app'
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    '@babel/plugin-proposal-class-properties'
+    ['@vue/app', {
+      decoratorsLegacy: true
+    }]
   ]
 }
