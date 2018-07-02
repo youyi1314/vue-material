22c145c488ccc980fa184bb60235cfbbc3b1bd5e
Marcos Moura <marcos.moura@maxmilhas.com.br>
2018-07-02 20:52:03 -0300

    refactor(Field): start creation of new field structure

--- packages/@vuematerial/_field/components/MdField.vue
+++ packages/@vuematerial/_field/components/MdField.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="md-field">
     <slot />
+
+    <div class="md-field-notch">
+      <span class="md-field-notch-mask left"/>
+      <span class="md-field-notch-mask right"/>
+    </div>
   </div>
 </template>
 
--- packages/@vuematerial/field/index.js
+++ packages/@vuematerial/field/index.js
@@ -1,9 +1,7 @@
 import bootstrap from '@vuematerial/core/bootstrap'
-import MdField from './components/MdField.vue'
-import MdInput from './components/MdInput.vue'
+import MdField from './components/MdField'
 
-export default (Vue) => {
+export default Vue => {
   bootstrap(Vue)
   Vue.component(MdField.name, MdField)
-  Vue.component(MdInput.name, MdInput)
 }
